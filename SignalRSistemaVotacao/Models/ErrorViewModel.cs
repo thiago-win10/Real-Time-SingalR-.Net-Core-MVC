44b680b24342cb3277407478a0afc22476b1c2d3
thiago-win10 <thiago.soares.dev@gmail.com>
2023-12-17 21:06:54 -0300

    Real Time MVC SingalR

---
+++ SignalRSistemaVotacao/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalRSistemaVotacao.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
